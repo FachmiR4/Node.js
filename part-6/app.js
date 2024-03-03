71f86badd165374c0168ef6d1a751ba53e117b11
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-03-03 17:32:43 +0700

    belajar membuat packages third party module (npm) by NodeJs part-8

---
+++ part-6/app.js
@@ -0,0 +1,7 @@
+const validator = require('validator');
+const chalk = require('chalk');
+
+const pesan =  chalk`Lorem, {bgGreen.k}`
+// console.log(validator.isEmail('fachmi ramadhan@gmail.com'))
+
+// npm i -g nodemonk
