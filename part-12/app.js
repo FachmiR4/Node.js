f724b4a9b800a02c13315de4307bd86414135e96
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-03-05 21:46:08 +0700

    belajar membuat middleware dan introduce Express middleware by nodeJS part-14

---
+++ part-12/app.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const expressLayout = require('express-ejs-layouts');
+const morgan = require('morgan');
+const app = express();
+const port = 3000
+// root = /
+// route = /about
+
+// gunakan ejs
+app.set('view engine', 'ejs');
+
+// third-party middleware
+app.use(expressLayout);
+app.use(morgan('dev'))
+
+//membuat built-in middleware
+app.use(express.static('public'));
+
+//Application-lavel middleware
+app.use((req, res, next) => {
+  console.log('time: ', Date.now());
+  next();
+})
+
+app.get('/', (req, res) => {
+  const mahasiswa = [
+    {
+      nama: 'fachmi ramadhan',
+      email: 'fachmi.F@gmail.com'
+    },
+    {
+      nama: 'Dodi supradi',
+      email: 'dodi.D@gmail.com'
+    },
+    {
+      nama: 'jayadi parmanan',
+      email: 'jayadi.J@gmail.com'
+    }
+  ]
+  res.render('index', {
+  nama: 'fachmi ramadhan', 
+  title: 'ini halaman home',
+  mahasiswa,
+  layout: 'layout/main-layout'});
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+      layout: 'layout/main-layout',
+      title: 'Halaman About'
+    })
+})
+app.get('/contact', function (req, res) {
+    res.render('contact', {
+      layout: 'layout/main-layout',
+      title: 'Halaman Contact'
+    })
+  })
+
+//use = digunakan untuk menjalakna middleware
+app.use('/',(req, res)=>{
+    res.status(404)
+    res.send('<h1>404</h1>')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
