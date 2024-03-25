8f3ec86d25e2ec5cbb49ff0aba2b5c4f098c5f9e
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-03-25 22:15:28 +0700

    belajar middleware pt.1 part-19

---
+++ part-13/middleware/logEvent.js
@@ -0,0 +1,24 @@
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+const fs = require('fs');
+const fsPromise = require('fs').promises;
+const path = require('path');
+const { log } = require('console');
+
+const logEvent = async (massage) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${dateTime}\t${uuid()}\t${massage}\n`;
+    console.log(logItem);
+    try{
+        if(!fs.existsSync(path.join(__dirname,'..', "logs"))){
+            await fsPromise.mkdir(path.join(__dirname,'..','logs'));
+        }
+        // testing
+        await fsPromise.appendFile(path.join(__dirname,'..',"logs", 'eventLog.txt'), logItem);
+    }catch(err){
+        console.error(err);
+    }
+}
+
+module.exports = logEvent;
--- part-13/server.js
+++ part-13/server.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
 const path =  require("path");
+const logEvent = require('./middleware/logEvent');
 const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use((req, res, next) => {
+    logEvent(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqlog.txt');
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+
+// built-in middleware to handle urlencoded data
+// in other words, form data:
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({extended: false}));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static filter
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('^/$|/index(.html)?', (req, res) => {
     // res.sendFile('./view/index.html', {root: __dirname});
