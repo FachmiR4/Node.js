18fae12312a732094b61d3285ab0964384c2e9c8
FachmiR4 <fachmiramadhan.fr@gmail.com>
2024-03-26 10:24:51 +0700

    belajar middleware pt.2 part-20

--- part-13/middleware/logEvent.js
+++ part-13/middleware/logEvent.js
@@ -4,9 +4,8 @@ const { v4: uuid } = require('uuid');
 const fs = require('fs');
 const fsPromise = require('fs').promises;
 const path = require('path');
-const { log } = require('console');
 
-const logEvent = async (massage) => {
+const logEvent = async (massage, nameLog) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${massage}\n`;
     console.log(logItem);
@@ -15,10 +14,16 @@ const logEvent = async (massage) => {
             await fsPromise.mkdir(path.join(__dirname,'..','logs'));
         }
         // testing
-        await fsPromise.appendFile(path.join(__dirname,'..',"logs", 'eventLog.txt'), logItem);
+        await fsPromise.appendFile(path.join(__dirname,'..',"logs", nameLog), logItem);
     }catch(err){
         console.error(err);
     }
 }
 
-module.exports = logEvent;
+const logger = (req, res, next) => {
+    logEvent(`${req.method}\t${req.headers.origin}\t${req.url}`, 'eventLog.txt');
+    console.log(`${req.method} ${req.path}`);
+    next();
+}
+
+module.exports = {logEvent, logger};
--- part-13/server.js
+++ part-13/server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
 const path =  require("path");
-const logEvent = require('./middleware/logEvent');
+const cors = require('cors')
+const { logger } = require('./middleware/logEvent');
+const errorHandler  = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
-app.use((req, res, next) => {
-    logEvent(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqlog.txt');
-    console.log(`${req.method} ${req.path}`);
-    next();
-});
+app.use(logger);
+
+// cross origins resource sharing
+const whitelist = ['https://www.google.com', "http://127.0.0.1:5500", "http://localhost:3500"];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) !== -1 || !origin){
+            callback(null, true);
+        }else{
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+}
+app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
 // in other words, form data:
@@ -38,7 +49,17 @@ app.get('/hello(.html)?', (req, res, next) => {
     res.send("heloo world");
 });
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "view", '404.html'));
+app.all('/*', (req, res) => {
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, "view", '404.html'));
+    }else if(req.accepts('json')){
+        res.json({error: "404 Not Found"});
+    }else {
+        res.type('txt').send('404 Not Found');
+    }
 });
+
+app.use(errorHandler);
+
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
